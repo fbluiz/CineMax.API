040356b36d7b754a3bb368e784dabdb2fe1da1ee
Fábio Luiz <96392802+fbluiz@users.noreply.github.com>
2023-01-24 22:06:28 -0300

    Repositório Base implementado

---
+++ CineMax.Core/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace CineMax.Core.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<List<T>> GetAsync();
+        Task<T> GetByIdAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ CineMax.Infra/Persistence/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using CineMax.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineMax.Infra.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ICineMaxDbContext _dbContext;
+
+        public Repository(ICineMaxDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public async Task<List<T>> GetAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().AsNoTracking().SingleOrDefaultAsync(predicate);
+        }
+        
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.Set<T>().Update(entity);
+        }
+    }
+}
