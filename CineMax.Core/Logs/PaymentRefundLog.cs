61f780c835d3c93723d34745b46e3c3fe65c41fe
FB <fbluiz.sf@gmail.com>
2023-06-20 21:54:42 -0300

    Log

---
+++ CineMax.Core/Logs/PaymentRefundLog.cs
@@ -0,0 +1,23 @@
+﻿using CineMax.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CineMax.Core.Logs
+{
+    public class PaymentRefundLog 
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public int ClientId { get; set; }
+        public string LogMessage { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public PaymentRefundLog(int ticketId, int clientId, string logMessage)
+        {
+            TicketId = ticketId;
+            ClientId = clientId;
+            LogMessage = logMessage;
+            CreatedOn = DateTime.Now;
+        }
+    }
+}
