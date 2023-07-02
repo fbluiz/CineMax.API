eecab14fc3c08c76f2b161c4df2f901ce48da281
FB <fbluiz.sf@gmail.com>
2023-07-02 18:04:42 -0300

    Extension Method para configurar as injeções de dependência

---
+++ CineMax.API/IoC/NativeInjectorConfig.cs
@@ -0,0 +1,47 @@
+﻿using CineMax.Application.Commands.CreateUser;
+using CineMax.Core.Auth;
+using CineMax.Core.Interfaces;
+using CineMax.Core.Repositories;
+using CineMax.Core.Services;
+using CineMax.Infra.Auth.Data;
+using CineMax.Infra.Persistence;
+using CineMax.Infra.Persistence.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineMax.API.IoC
+{
+    public static class NativeInjectorConfig
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CineMaxDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("CineMaxCsFb"))
+            );
+
+            services.AddDbContext<IdentityDataContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("CineMaxCsFb"))
+            );
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDataContext>()
+                .AddDefaultTokenProviders();
+
+           services.AddScoped<IAuthService, AuthService>();
+
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+            services.AddScoped<ISectionRepository, SectionRepository>();
+            services.AddScoped<ISectionSeatRepository, SectionSeatRepository>();
+            services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<IPaymentRefundLogRepository, PaymentRefundLogRepository>();
+            services.AddScoped<IPaymentService, PaymentService>();
+
+            services.AddMediatR(typeof(CreateUserCommand));
+
+        }
+    }
+}
--- CineMax.API/Program.cs
+++ CineMax.API/Program.cs
@@ -1,59 +1,17 @@
 using CineMax.API.Extensions;
-using CineMax.Application.Commands.CreateUser;
-using CineMax.Core.Auth;
-using CineMax.Core.Interfaces;
-using CineMax.Core.Repositories;
-using CineMax.Core.Services;
-using CineMax.Infra.Auth.Data;
-using CineMax.Infra.Persistence;
-using CineMax.Infra.Persistence.Repositories;
-using MediatR;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using CineMax.API.IoC;
 using Way2Commerce.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddVersioning();
 builder.Services.AddSwagger();
-
 builder.Services.AddAuthentication(builder.Configuration);
 builder.Services.AddAuthorization();
-
-var connectionString = builder.Configuration.GetConnectionString("CineMaxCsVini");
-// ATENÇÃO! Alterar a referência da string de conexão
-builder.Services.AddDbContext<CineMaxDbContext>(options => options
-.UseSqlServer(connectionString));
-builder.Services.AddDbContext<IdentityDataContext>(options =>
-    options.UseSqlServer(connectionString));
-
-
-builder.Services.AddMediatR(typeof(CreateUserCommand));
-
-builder.Services.AddDefaultIdentity<IdentityUser>()
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<IdentityDataContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-builder.Services.AddScoped<IMovieRepository, MovieRepository>(); 
-builder.Services.AddScoped<IClientRepository, ClientRepository>();  
-builder.Services.AddScoped<IRoomRepository, RoomRepository>();
-builder.Services.AddScoped<ISectionRepository, SectionRepository>();
-builder.Services.AddScoped<ISectionSeatRepository, SectionSeatRepository>();
-builder.Services.AddScoped<ITicketRepository, TicketRepository>();
-builder.Services.AddScoped<IPaymentRefundLogRepository, PaymentRefundLogRepository>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
-
-//Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
 {
